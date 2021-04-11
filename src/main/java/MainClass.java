eb8d2490388ec6a1a5733a3c2353446521a8bfd5
StaroverovAlan <75185102+StaroverovAlan@users.noreply.github.com>
2021-04-11 19:18:57 +0300

    Tests for yandex
    Автотесты для яндекса

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -5,28 +5,25 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class MainClass {
 
      static WebDriver driver;
+     static WebDriverWait wait;
      static String ChromeDriverProperty = "webdriver.chrome.driver";
      static String ChromeDriverPath = System.getProperty("user.dir") + "\\drivers\\chromedriver.exe";
-     static WebDriverWait wait;
-
 
     public static void main(String[] args) {
         System.setProperty(ChromeDriverProperty, ChromeDriverPath);
         driver = new ChromeDriver();
         wait = (new WebDriverWait(driver, 5));
-        GoogleMainPage googleMainPage = new GoogleMainPage();
-        GoogleLoginPage googleLoginPage = new GoogleLoginPage();
-        String login = "TestUserForSeleniumWD@gmail.com";
+        YandexMainPage yandexMainPage = new YandexMainPage(driver, wait);
+        YandexLoginPage yandexLoginPage = new YandexLoginPage(driver, wait);
+        String login = "TestUserJavaSelenium";
         String password = "Qwerty$4";
-        String userFirstName = "Test";
-        String userSecondName = "User";
 
-        googleMainPage.openGooglePage();
-        googleMainPage.goToLoginPage();
-        googleLoginPage.enterLogin(login);
-        googleLoginPage.enterPassword(password);
-        googleMainPage.isLoggedIn(userFirstName, userSecondName, login.toLowerCase());
-        System.out.println("Успешно!!!!");
+        yandexMainPage.openYandexPage();
+        yandexMainPage.goToLoginPage();
+        yandexLoginPage.enterLogin(login);
+        yandexLoginPage.enterPassword(password);
+        yandexMainPage.isLoggedIn(login);
+        System.out.println("Успешно!");
         driver.quit();
     }
 }
